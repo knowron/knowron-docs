08641e478aab4864ed779e3309b1a79e8ad1d2ac
Arturo Buitrago Méndez <arturo@knowron.com>
2022-11-30 13:45:32 +0100

    update of config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://knowron-docs.github.io',
-  baseUrl: '/',
+  baseUrl: '/knowron-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
